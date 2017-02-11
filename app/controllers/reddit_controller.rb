4cff15ebc21f25cf13ec9993cc8ba4af68981136
Doug Bennett <dben89x@gmail.com>
2017-02-10 21:39:41 -0700

    removing constants in reddit controller

--- app/controllers/reddit_controller.rb
+++ app/controllers/reddit_controller.rb
@@ -1,11 +1,11 @@
 class RedditController < ApplicationController
 
 	def index
-		CLIENT_ID = "XQGGfiz7TXSUKw"
-		SECRET = "ODfBTmT2_Cahr1FCT8AzvbAnaYM"
-		REDIRECT_URI = "http://railsdashboard.herokuapp.com"
-		w = Redd.it(:web, CLIENT_ID, SECRET, REDIRECT_URI, user_agent: "TestSite v1.0.0")
-		
+		client_id = "XQGGfiz7TXSUKw"
+		secret = "ODfBTmT2_Cahr1FCT8AzvbAnaYM"
+		redirect_uri = "http://railsdashboard.herokuapp.com"
+		w = Redd.it(:web, client_id, secret, redirect_uri, user_agent: "TestSite v1.0.0")
+
 		# w = Redd.it(:web, ENV["REDDIT_CLIENT"], ENV["REDDIT_SECRET"], ENV["REDDIT_REDIRECT"], user_agent: "TestSite v1.0.0")
 
 		url = w.auth_url("random_state", ["identity", "read"], :temporary)
